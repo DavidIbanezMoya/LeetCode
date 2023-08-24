37ed0fd6696b9d5b581cc6bde4d3339a9cddc158
DavidIbanezMoya <davidibanezmoya@gmail.com>
2023-08-24 10:30:09 +0200

    Single Number Working

---
+++ src/Top100/Misc/Main.java
@@ -0,0 +1,9 @@
+package Top100.Misc;
+
+public class Main {
+    public static void main(String[] args) {
+        SingleNumber singleNumber = new SingleNumber();
+        int[] nums = {2, 4, 6, 2, 4};
+        singleNumber.singleNumber(nums);
+    }
+}
---
+++ src/Top100/Misc/SingleNumber.java
@@ -0,0 +1,26 @@
+package Top100.Misc;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+
+        ArrayList<Integer> emptyNumber = new ArrayList<>();
+        //Adding every number to an ArrayList
+        for (int i = 0; i < nums.length; i++) {
+            emptyNumber.add(nums[i]);
+            int count = 0;
+            for (int j = 0; j < emptyNumber.size(); j++) {
+                if (emptyNumber.get(j) == nums[i]) {
+                    count++;
+                }
+                //If the number is repeated we will delete all of those
+                if (count > 1){
+                    emptyNumber.removeAll(Collections.singleton(Integer.valueOf(emptyNumber.get(j))));
+                }
+            }
+        }
+        return emptyNumber.get(0);
+    }
+}
