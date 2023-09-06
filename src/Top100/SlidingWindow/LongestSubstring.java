57e04d4c95b30e7b87944b1dd725684cabe41e23
DavidIbanezMoya <davidibanezmoya@gmail.com>
2023-09-06 21:31:43 +0200

    Starting LongestSubstring

---
+++ src/Top100/SlidingWindow/LongestSubstring.java
@@ -0,0 +1,17 @@
+package Top100.SlidingWindow;
+
+//Given a string s, find the length of the longest
+//substring
+// without repeating characters.
+
+public class LongestSubstring {
+    public int lengthOfLongestSubstring(String s) {
+        int length = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            //Check for the sliding window method
+
+        }
+        return length;
+    };
+}
---
+++ src/Top100/SlidingWindow/Main.java
@@ -0,0 +1,9 @@
+package Top100.SlidingWindow;
+
+public class Main {
+    public static void main(String[] args) {
+        LongestSubstring longestSubstring = new LongestSubstring();
+        longestSubstring.lengthOfLongestSubstring("abcabcbb");
+    }
+
+}
