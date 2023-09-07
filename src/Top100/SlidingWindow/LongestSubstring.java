bcb0205037a15a9ba11f605cb0a3b0b52c6fe602
DavidIbanezMoya <davidibanezmoya@gmail.com>
2023-09-07 14:38:12 +0200

    LongestSubstring working

--- src/Top100/SlidingWindow/LongestSubstring.java
+++ src/Top100/SlidingWindow/LongestSubstring.java
@@ -8,48 +8,39 @@ import java.util.ArrayList;
 
 public class LongestSubstring {
     public int lengthOfLongestSubstring(String s) {
+        if (s.length() == 0) return 0;
+
         int length = 1;
         int maxLength = length;
 
         int n = 0;
         String currentWord = "";
 
-        ArrayList<String>test = new ArrayList<>();
         //Use a while to move the widnow
-        while (n < s.length()-1) {
+        while (n <= s.length()-1) {
             if (currentWord.contains(s.substring(n,n+1))) {
-                if (length > maxLength) maxLength = length-1;
+                if (length > maxLength) maxLength = length;
 
                 //Check the position where the first character is to start from there +1
-                if (currentWord.length() > 1) {
+                if (currentWord.length() >= 1) {
                     currentWord += s.substring(n,n+1);
+                    currentWord = currentWord.substring(currentWord.indexOf(s.substring(n,n+1))+1);
 
-                    //Check where is the letter that we want to remove
-                    System.out.println(currentWord.substring(currentWord.indexOf(s.substring(n,n+1))+1));
-                    System.out.println(currentWord.split(s.substring(n,n+1))[1]);
-                    System.out.println(s.substring(n,n+1));
-
-                    if ( currentWord.split(s.substring(n,n+1))[0].contains(s.substring(n,n+1))) {
-                        currentWord = currentWord.split(s.substring(n,n+1))[1];
-                        //If the letter is in the first half then we will pick the second one
-                    } else {
-                        System.out.println(currentWord.split(s.substring(n, n + 1))[0]);
-                        //todo Haig de pensar en una forma d'agafar el fragment amb la lletra mes nova
-
-                        currentWord = currentWord.split(s.substring(n, n + 1))[0];
-                    }
                 } else {
                     currentWord = "";
                 }
                 length = currentWord.length();
+                n++;
             }
             else {
+                //Adding a new char to the word
                 currentWord += s.substring(n,n+1);
-                length++;
+                if (n >= 1) length++;
                 n++;
             }
         }
 
+        if (length > maxLength) maxLength = length;
         return maxLength;
     };
 }
