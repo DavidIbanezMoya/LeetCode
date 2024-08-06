d3c8cbb85f4b76481c659c426428bc022cc24e0c
DavidIbanezMoya <davidibanezmoya@gmail.com>
2024-08-06 19:54:16 +0200

    Created C# Project

---
+++ src/TopInterviewQuestions/Easy/Array/MainArray.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.src.TopInterviewQuestions.Easy.Array
+{
+    class MainArray
+    {
+        public static void Main(string[] args)
+        {
+            RemoveDuplicates removeDuplicates = new RemoveDuplicates();
+            removeDuplicates.RemoveDuplicatesSolution(new int[] { 1,2,3});
+        }
+    }
+}
