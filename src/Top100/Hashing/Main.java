f905669d1a7a74a1045e425e236e24ab0f3b782b
DavidIbanezMoya <davidibanezmoya@gmail.com>
2024-08-06 19:16:19 +0200

    Vscode

--- src/Top100/Hashing/Main.java
+++ src/Top100/Hashing/Main.java
@@ -2,8 +2,13 @@ package Top100.Hashing;
 
 public class Main {
     public static void main(String[] args) {
-        TwoSum sum = new TwoSum();
+        /*TwoSum sum = new TwoSum();
         int[] nums = {0,4,3,0};
-        System.out.println(sum.twoSum(nums,0));
+        System.out.println(sum.twoSum(nums,0));*/
+
+        GroupAnagrams anagrams = new GroupAnagrams();
+        String[] strs = {"eat","tea","tan","ate","nat","bat"};
+        anagrams.groupAnagrams(strs);
+
     }
 }
